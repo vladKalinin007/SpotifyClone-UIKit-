17323b948357f2023b72a65dd2ddc6307db6d703
vladKalinin007 <109882612+vladKalinin007@users.noreply.github.com>
2023-02-04 01:36:39 +0200

    Update SearchResultsViewController.swift

--- Spotify/Controllers/Other/SearchResultsViewController.swift
+++ Spotify/Controllers/Other/SearchResultsViewController.swift
@@ -7,26 +7,10 @@
 
 import UIKit
 
-// MARK: - MAIN
-
 class SearchResultsViewController: UIViewController {
-    
-// MARK: - PROPERTIES
-    
-    
-    
-// MARK: - LIFECYCLES
-    
-    // MARK: View is loaded into memory
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
-    }
-    
-// MARK: - FUNCTIONS
-    
-    
-    
+    }  
 }
