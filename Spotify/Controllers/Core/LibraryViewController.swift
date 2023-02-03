8147dc937fe4e9f2e26052eb883b8ede3c84137b
vladKalinin007 <109882612+vladKalinin007@users.noreply.github.com>
2023-02-04 01:30:38 +0200

    Update LibraryViewController.swift

--- Spotify/Controllers/Core/LibraryViewController.swift
+++ Spotify/Controllers/Core/LibraryViewController.swift
@@ -7,27 +7,15 @@
 
 import UIKit
 
-// MARK: - MAIN
+
 
 class LibraryViewController: UIViewController {
     
-// MARK: - PROPERTIES
-    
-    
-    
-// MARK: - LIFECYCLES
-    
-    // MARK: View is loaded into memory
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Library"
         view.backgroundColor = .systemBackground
         
-    }
-    
-// MARK: - FUNCTIONS
-    
-    
-    
+    }   
 }
